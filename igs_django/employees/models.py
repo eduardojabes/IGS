7d3cc42c83c1a41a5816a471711a6b9946ad2702
eduardojabes <eduardojabes@hotmail.com>
2022-09-01 21:31:30 -0300

    feat: created models

--- igs_django/employees/models.py
+++ igs_django/employees/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from igs_django.departments.models import Departments
+
 # Create your models here.
+class Employees(models.Model):
+    department_id = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.IntegerField(default=0)
